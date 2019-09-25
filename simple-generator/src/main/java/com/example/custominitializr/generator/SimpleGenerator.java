ea1b193a7badc15eeab60438cdb87e9f07fc47dc
Stephane Nicoll <snicoll@pivotal.io>
2019-09-25 11:29:51 +0200

    ProjectContributor demonstration
    This commit adds a simple `ProjectContributor` to our generator that
    creates a file at the root of the project structure. Right now, the
    contributor is registered programmatically.

--- simple-generator/src/main/java/com/example/custominitializr/generator/SimpleGenerator.java
+++ simple-generator/src/main/java/com/example/custominitializr/generator/SimpleGenerator.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import com.example.custominitializr.generator.contributor.SimpleContributor;
 import io.spring.initializr.generator.project.DefaultProjectAssetGenerator;
 import io.spring.initializr.generator.project.ProjectAssetGenerator;
 import io.spring.initializr.generator.project.ProjectDescription;
@@ -16,7 +17,7 @@ class SimpleGenerator {
 
 	Path generateProject(ProjectDescription description) {
 		ProjectGenerator projectGenerator = new ProjectGenerator((context) -> {
-
+			context.registerBean(SimpleContributor.class, SimpleContributor::new);
 		});
 		ProjectAssetGenerator<Path> projectAssetGenerator = new DefaultProjectAssetGenerator(
 				(resolvedDescription) -> Paths.get("target/projects",
